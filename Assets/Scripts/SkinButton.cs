ae3f227ac599c0b85ba5e556ea883436404561da
Jesus Castro <jjoesei.castro@gmail.com>
2025-06-10 23:24:50 +0800

    Add skin names

--- Assets/Scripts/SkinButton.cs
+++ Assets/Scripts/SkinButton.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -8,8 +9,12 @@ public class SkinButton : MonoBehaviour {
     [SerializeField]
     private Image image;
 
+    [SerializeField]
+    private TextMeshProUGUI skinName;
+
     public void SetSkin(Skin skin, UnityAction buttonAction) {
         this.image.sprite = skin.midSprite;
+        this.skinName.text = skin.id;
         
         this.button.onClick.AddListener(buttonAction);
     }
